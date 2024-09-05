c6cb4c4845eefdbfb7010b4a618cbf33d6cc6e8a
nicholas.berardo <berardonicholas5@gmail.com>
2024-09-05 09:58:35 +0200

    run make.sh

---
+++ evaluation_custom_datasets/register_RA.py
@@ -0,0 +1,34 @@
+import cv2
+import numpy as np
+import torch
+from PIL import Image
+from detectron2.config import get_cfg
+from detectron2.engine import DefaultTrainer
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.projects.deeplab import add_deeplab_config
+
+from mask2former import add_maskformer2_config
+
+"""x = cv2.imread("../../../legion/Datasets/FS_LostFound_full/labels_masks/54.png")
+# RA21 -> 0:Cityscapes 1:Anomaly 255:VOID
+# RO21 -> 0:Street 1:Anomaly 255:Other things
+# RO   -> 0:Ciryscapes 2:Anomaly
+# FS_S -> 0:Cityscapes 1:Anomaly 255:VOID
+# FS_L&F -> 0:Cityscapes 1:Anomaly 255:VOID
+print(np.unique(x))
+x = np.where((x==0), 0, x)
+x = np.where((x==1), 100, x)
+x = np.where((x==255), 255, x)
+cv2.imshow("image",x)
+cv2.waitKey(0)cd 
+cv2.destroyAllWindows()"""
+
+cfg = get_cfg()
+# for poly lr schedule
+add_deeplab_config(cfg)
+add_maskformer2_config(cfg)
+cfg.merge_from_file("configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k.yaml")
+cfg.freeze()
+print(cfg)
+#model = torch.load("../../../legion/model_0079999.pth", weights_only=False, map_location='cpu')["model"]
+#img = Image.open("../../../legion/Datasets/FS_LostFound_full/images/54.png")
