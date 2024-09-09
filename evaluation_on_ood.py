30e0f9dc336aca96ec1a7ef6cd5a27d330b1df54
nicholas.berardo <berardonicholas5@gmail.com>
2024-09-09 10:10:27 +0200

    run make.sh

--- evaluation_on_ood.py
+++ evaluation_on_ood.py
@@ -61,8 +61,9 @@ if __name__=="__main__":
         img = read_image(img_path, format="BGR")
         img = img.reshape((img.shape[2], img.shape[0], img.shape[1]))
         input = [{"image": torch.tensor(img).float(), "height": img.shape[1], "width": img.shape[2]}]
-        prediction = model(input)
-        print(prediction[0]["sem_seg"].shape)
+        prediction = model(input)[0]["sem_seg"] #Here C = 19, cityscapes classes
+        prediction = torch.max(prediction, axis=1)
+        print(prediction.shape)
 
         pathGT = img_path.replace("images", "labels_masks")
 
