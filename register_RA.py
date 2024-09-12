7a88480ae33d276ee770e83d36d5878307793603
nicholas.berardo <berardonicholas5@gmail.com>
2024-09-12 13:41:20 +0200

    run make.sh

--- register_RA.py
+++ register_RA.py
@@ -11,6 +11,7 @@ from detectron2.projects.deeplab import add_deeplab_config
 from detectron2.utils.logger import setup_logger
 from mask2former import add_maskformer2_config
 import detectron2.utils.comm as comm
+import argparse
 
 """x = cv2.imread("../../../legion/Datasets/FS_LostFound_full/labels_masks/54.png")
 # RA21 -> 0:Cityscapes 1:Anomaly 255:VOID
@@ -26,30 +27,27 @@ cv2.imshow("image",x)
 cv2.waitKey(0)cd 
 cv2.destroyAllWindows()"""
 
-"""
 cfg = get_cfg()
 # for poly lr schedule
 add_deeplab_config(cfg)
 add_maskformer2_config(cfg)
 cfg.merge_from_file("configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k.yaml")
 cfg.freeze()
-print(cfg)
-default_setup(cfg, "")
-setup_logger(output=cfg.OUTPUT_DIR, distributed_rank=comm.get_rank(), name="mask2former")
+
+setup_logger(name="fvcore")
+logger = setup_logger()
+
 model = DefaultTrainer.build_model(cfg)
-DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(
+DetectionCheckpointer(model).resume_or_load(
             cfg.MODEL.WEIGHTS, resume=False)
-#model = torch.load("../../../legion/model_0079999.pth", weights_only=False, map_location='cpu')["model"]
+
 #read_image return HWC
 img = read_image("/home/nberardo/Datasets/FS_LostFound_full/images/54.png", format="BGR")
-#print(model.state_dict().keys())
+
 img = img.reshape((img.shape[2], img.shape[0], img.shape[1]))
 print(img.shape)
 input = [{"image": torch.tensor(img).float(), "height": img.shape[1], "width": img.shape[2]}]
 model.training = False
-res = model(input)
-print(res[0]["sem_seg"].shape)"""
+res = model(input)[0]["sem_seg"]
+print(res.shape)
 
-model = torch.load("backbone_weights/vicreg_resnet50.pkl")
-for n,w in model.items():
-    print(n)
