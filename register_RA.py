7a1eb00e41d98d046d3e738e55af8b341b778b95
nicholas.berardo <berardonicholas5@gmail.com>
2024-09-12 14:54:22 +0200

    run make.sh

--- register_RA.py
+++ register_RA.py
@@ -96,6 +96,6 @@ print(res.shape)
 res = torch.max(res,axis=1)[0]
 print(res.shape)
 
-im = Image.fromarray(decode_segmap(res.cpu().numpy()))
+im = Image.fromarray(decode_segmap(res.detach().cpu().numpy()))
 im.save("/home/nberardo/mask2former/image_results/img.jpg")
 
