199a6082830aed67c4b4c6a139981552424ef12e
nicholas.berardo <berardonicholas5@gmail.com>
2024-10-26 10:45:19 +0200

    component metric

--- evaluation_on_id.py
+++ evaluation_on_id.py
@@ -11,18 +11,5 @@ if __name__ == '__main__':
     cfg = setup(args)
     model = Trainer.build_model(cfg)
     DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(cfg.MODEL.WEIGHTS,resume=args.resume)
-    model.eval()
-    # res = Trainer.test(cfg,model)
-    for d in cfg.DATASETS.TEST:
-        data_loader = Trainer.build_test_loader(cfg,d)
-        for input in data_loader:
-            gt = input[0]["sem_seg"].detach().cpu().numpy()
-            output = model(input)[0]["sem_seg"].unsqueeze(0)
-            pred = torch.max(output,axis=1)[1]
-            pred = pred.detach().cpu().numpy().squeeze()
-            print(numpy.unique(pred,return_counts=True))
-            gt_instances , pred_instances =  anomaly_instances_from_mask(pred,gt)
-            metric = segment_metrics(gt_instances,pred_instances)
-            print(metric)
-            break
+    res = Trainer.test(cfg,model)
 
