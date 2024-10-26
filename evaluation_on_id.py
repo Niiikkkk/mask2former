73b99ac33522228f8abd2a06ca44710cda6ce74f
nicholas.berardo <berardonicholas5@gmail.com>
2024-10-26 09:46:54 +0200

    component metric

---
+++ evaluation_on_id.py
@@ -0,0 +1,12 @@
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.engine import default_argument_parser
+
+from train_net import Trainer, setup
+
+if __name__ == '__main__':
+    args = default_argument_parser().parse_args()
+    cfg = setup(args)
+    model = Trainer.build_model(cfg)
+    DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(cfg.MODEL.WEIGHTS,resume=args.resume)
+    res = Trainer.test(cfg,model)
+    print(res)
