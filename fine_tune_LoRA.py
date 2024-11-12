4b04bccb727b9adf94e72f34f4f6ebf3a4fbb7c6
nicholas.berardo <berardonicholas5@gmail.com>
2024-11-12 11:42:16 +0100

    component metric

--- fine_tune_LoRA.py
+++ fine_tune_LoRA.py
@@ -31,6 +31,9 @@ def print_named_modules(model):
         print(name, module)
 
 def main(args):
+    model = torch.load(args.OUTPUT_DIR + "/model_final.pth")
+    print(model)
+    return
     cfg = setup(args)
     trainer = Trainer(cfg)
     trainer.resume_or_load(resume=args.resume)
