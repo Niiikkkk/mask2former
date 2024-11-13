8e0213e8453af1cd0b008730b8cfcb3033b73f2f
nicholas.berardo <berardonicholas5@gmail.com>
2024-11-13 11:48:53 +0100

    component metric

---
+++ evaluation_lora.py
@@ -0,0 +1,20 @@
+import numpy
+import torch
+from detectron2.checkpoint import DetectionCheckpointer
+from detectron2.engine import default_argument_parser
+from component_metric import get_threshold_from_PRC, segment_metrics, default_instancer, anomaly_instances_from_mask
+from peft import PeftConfig, PeftModel
+from train_net import Trainer, setup
+
+if __name__ == '__main__':
+    args = default_argument_parser().parse_args()
+    cfg = setup(args)
+    model = Trainer.build_model(cfg)
+    DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(cfg.MODEL.WEIGHTS,resume=args.resume)
+
+    model_id = cfg.OUTPUT_DIR + "/lora_model"
+
+    inference_model = PeftModel.from_pretrained(model,model_id)
+
+    res = Trainer.test(cfg,inference_model)
+    print(res)
