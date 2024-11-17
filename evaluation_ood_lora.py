b7c8a55f2e5c39c2b35295ddb273c705f3331c8a
nicholas.berardo <berardonicholas5@gmail.com>
2024-11-17 11:55:10 +0100

    component metric

--- evaluation_ood_lora.py
+++ evaluation_ood_lora.py
@@ -30,6 +30,6 @@ if __name__ == '__main__':
     predictor.model = inference_model
     # inference_model = PeftModel.from_pretrained(model,model_id)
 
-    func(predictor,args,cfg)
-    #res = Trainer.test(cfg,inference_model)
+    # func(predictor,args,cfg)
+    res = Trainer.test(cfg,predictor)
 
