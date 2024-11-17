c269810c3d042b93b57955ffb8fca30fee612efc
nicholas.berardo <berardonicholas5@gmail.com>
2024-11-17 10:35:51 +0100

    component metric

--- evaluation_ood_lora.py
+++ evaluation_ood_lora.py
@@ -13,9 +13,9 @@ if __name__ == '__main__':
     args = parse_args()
     cfg = setup(args)
     model = Trainer.build_model(cfg)
-    DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(cfg.MODEL.WEIGHTS,resume=args.resume)
+    DetectionCheckpointer(model, save_dir=cfg.OUTPUT_DIR).resume_or_load(cfg.MODEL.BASE_WEIGHTS,resume=args.resume)
 
-    model_id = os.path.join(cfg.OUTPUT_DIR, "lora_model")
+    model_id = cfg.MODEL.LORA_WEIGHTS
 
     lora_config = LoraConfig.from_pretrained(model_id)
 
