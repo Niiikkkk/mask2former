c5fda044b7b2579d0e9aad76aa0fcd31d3b8f51c
nicholas.berardo <berardonicholas5@gmail.com>
2024-11-29 15:00:01 +0100

    print_some_images

--- code_for_stuff.sh
+++ code_for_stuff.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-export DETECTRON2_DATASETS=/home/nberardo/Datasets
-export CUDA_HOME=/usr/local/cuda-11.3
-
 python code_for_stuff.py \
   --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k.yaml \
   --input \
