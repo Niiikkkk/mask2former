f66f6118d6d78466b87719568db7df10f56ce3d2
nicholas.berardo <berardonicholas5@gmail.com>
2024-11-29 18:44:34 +0100

    print_some_images

--- code_for_stuff.py
+++ code_for_stuff.py
@@ -93,7 +93,8 @@ def print_img(image_to_plot,path_to_save):
     if "image" in path_to_save:
         plt.imshow(cv2.cvtColor(image_to_plot, cv2.COLOR_BGR2RGB))
     else:
-        print(path_to_save)
+        if "label" in path_to_save:
+            image_to_plot = torch.permute(image_to_plot,(2,0,1))
         plt.imshow(decode_segmap(image_to_plot))
     plt.savefig(path_to_save)
     plt.close()
