daa59fd10eefc05596bfdc1eaddc9de7b012e51d
nicholas.berardo <berardonicholas5@gmail.com>
2024-12-01 14:15:54 +0100

    print_some_images

--- train_bt.sh
+++ train_bt.sh
@@ -8,4 +8,4 @@ export CUDA_HOME=/usr/local/cuda-11.3
 
 python train_net.py \
   --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_bt.yaml \
-  --num-gpus 1 --resume
+  --num-gpus 2
