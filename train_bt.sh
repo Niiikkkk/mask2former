6b0a5f2d9608ba4d91fa5e0e2905a9b7bb51781d
nicholas.berardo <berardonicholas5@gmail.com>
2024-12-01 11:26:31 +0100

    print_some_images

---
+++ train_bt.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+source /home/nberardo/miniconda3/bin/activate
+
+#export DETECTRON2_DATASETS=/home/nberardo/Datasets
+#export CUDA_HOME=/home/nberardo/cuda_11.8
+
+#convert pth weights to pkl
+#python tools/convert-torchvision-to-d2.py /home/nberardo/simsiam/resnet50.pth /home/nberardo/mask2anomaly_v2/backbone_weights/simsiam_resnet50.pkl
+
+python train_net.py \
+  --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_bt.yaml \
+  --num-gpus 2
