c5fc7ce944446c391b065efb0cf7130e7ec52eb2
nicholas.berardo <berardonicholas5@gmail.com>
2024-12-11 13:22:07 +0100

    print_some_images

--- eval_ood.sh
+++ eval_ood.sh
@@ -3,17 +3,17 @@
 export DETECTRON2_DATASETS=/home/nberardo/Datasets
 export CUDA_HOME=/usr/local/cuda-11.3
 
-python evaluation_on_ood.py --input /home/nberardo/Datasets/RoadAnomaly21/images/*.png \
- --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml
-
-python evaluation_on_ood.py --input /home/nberardo/Datasets/RoadAnomaly/images/*.jpg \
- --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml
-
-python evaluation_on_ood.py --input /home/nberardo/Datasets/RoadObsticle21/images/*.webp \
- --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml
-
-python evaluation_on_ood.py --input /home/nberardo/Datasets/FS_LostFound_full/images/*.png \
- --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml
+#python evaluation_on_ood.py --input /home/nberardo/Datasets/RoadAnomaly21/images/*.png \
+# --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml
+#
+#python evaluation_on_ood.py --input /home/nberardo/Datasets/RoadAnomaly/images/*.jpg \
+# --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml
+#
+#python evaluation_on_ood.py --input /home/nberardo/Datasets/RoadObsticle21/images/*.webp \
+# --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml
+#
+#python evaluation_on_ood.py --input /home/nberardo/Datasets/FS_LostFound_full/images/*.png \
+# --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml
 
 python evaluation_on_ood.py --input /home/nberardo/Datasets/fs_static/images/*.jpg \
  --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml
