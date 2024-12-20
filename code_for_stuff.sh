d89b4ed5290c768ef79c504879cf7f60a6350831
nicholas.berardo <berardonicholas5@gmail.com>
2024-12-20 19:47:12 +0100

    print_some_images

--- code_for_stuff.sh
+++ code_for_stuff.sh
@@ -7,5 +7,5 @@
 
 python code_for_stuff.py \
   --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
-  --input /home/nberardo/Datasets/RoadAnomaly/images/*.jpg \
+  --input /home/nberardo/Datasets/RoadObsticle21/images/*.webp \
 
