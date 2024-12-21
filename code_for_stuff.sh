95e24f7c4766109e9a476a3e349d56862010cd0a
nicholas.berardo <berardonicholas5@gmail.com>
2024-12-21 21:22:36 +0100

    print_some_images

--- code_for_stuff.sh
+++ code_for_stuff.sh
@@ -7,5 +7,5 @@
 
 python code_for_stuff.py \
   --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
-  --input /home/nberardo/Datasets/FS_LostFound_full/images/*.png \
+  --input /home/nberardo/Datasets/fs_static/images/*.jpg \
 
