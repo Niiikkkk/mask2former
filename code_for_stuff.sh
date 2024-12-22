df93288ba680324201c571aec3b87b074fdba87f
nicholas.berardo <berardonicholas5@gmail.com>
2024-12-22 17:45:35 +0100

    print_some_images

--- code_for_stuff.sh
+++ code_for_stuff.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-#python code_for_stuff.py \
-#  --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
-#  --input /home/nberardo/Datasets/RoadAnomaly21/images/*.png \
+python code_for_stuff.py \
+  --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
+  --input /home/nberardo/Datasets/RoadAnomaly21/images/*.png \
 
 #python code_for_stuff.py \
 #--config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
 #--input /home/nberardo/Datasets/RoadAnomaly/images/*.jpg \
 #
-python code_for_stuff.py \
---config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
---input /home/nberardo/Datasets/RoadObsticle21/images/*.webp \
+#python code_for_stuff.py \
+#--config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
+#--input /home/nberardo/Datasets/RoadObsticle21/images/*.webp \
 
 #python code_for_stuff.py \
 #--config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
