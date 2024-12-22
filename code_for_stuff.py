76ff9a01a501a3692a6853d58ca73a6c45889341
nicholas.berardo <berardonicholas5@gmail.com>
2024-12-22 14:58:24 +0100

    print_some_images

--- code_for_stuff.py
+++ code_for_stuff.py
@@ -129,7 +129,7 @@ if __name__ == "__main__":
     cfg = setup_cfgs(args)
 
     model = DefaultPredictor(cfg)
-    print([p for p in model.model.parameters()])
+    print([n for n,p in model.model.named_parameters()])
     num_train_param = sum(p.numel() for p in model.model.backbone.parameters() if p.requires_grad)
     print(num_train_param)
 
