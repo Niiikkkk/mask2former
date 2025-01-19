e6140ae203522626f095323b202994189f5ef8ea
nicholas.berardo <berardonicholas5@gmail.com>
2025-01-19 22:57:47 +0100

    print_some_images

--- fine_tune_lora.sh
+++ fine_tune_lora.sh
@@ -4,5 +4,5 @@ export DETECTRON2_DATASETS=/home/nberardo/Datasets
 export CUDA_HOME=/usr/local/cuda-11.3
 
 python fine_tune_LoRA.py \
-  --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_bt.yaml \
+  --config-file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_bt.yaml \
   --num-gpus 1
