0ccc083764cce99c1bd57482b31bedcf674e8cfc
nicholas.berardo <berardonicholas5@gmail.com>
2025-01-22 08:49:29 +0100

    print_some_images

--- fine_tune_LoRA.py
+++ fine_tune_LoRA.py
@@ -514,7 +514,7 @@ def main(args):
                        r"|backbone\.res\d\.\d\.conv\d",
         lora_dropout=0.1,
         bias="lora_only",
-        modules_to_save=["predictor"],
+        modules_to_save=["sem_seg_head.predictor"],
     )
 
     lora_model = get_peft_model(model,lora_cfg_old)
