4282c8a767b5883222b1bca4ff0bba4090ffd215
nicholas.berardo <berardonicholas5@gmail.com>
2025-01-24 12:07:36 +0100

    print_some_images

--- eval_ood_lora.sh
+++ eval_ood_lora.sh
@@ -3,7 +3,7 @@ export DETECTRON2_DATASETS=/home/nberardo/Datasets
 export CUDA_HOME=/usr/local/cuda-11.3
 
 python evaluation_ood_lora.py --input /home/nberardo/Datasets/RoadAnomaly21/images/*.png \
- --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_bt.yaml
+ --config-file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_bt.yaml
 
 #python evaluation_ood_lora.py --input /home/nberardo/Datasets/RoadAnomaly/images/*.jpg \
 # --config_file /home/nberardo/mask2former/configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference_lora.yaml
