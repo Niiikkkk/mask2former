397d8845c5d6c7d69a5fd69034cc4cc483362d8d
nicholas.berardo <berardonicholas5@gmail.com>
2025-01-28 17:17:01 +0100

    print_some_images

--- fine_tune_LoRA.py
+++ fine_tune_LoRA.py
@@ -100,6 +100,7 @@ def main(args):
 
     lora_model.print_trainable_parameters()
     print_total_params(lora_model)
+    print("CIAO")
     return
 
     optimizer = trainer.build_optimizer(cfg, lora_model)
