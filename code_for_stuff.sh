6f63bf05b45684a51aa638175f182a34dad1b668
nicholas.berardo <berardonicholas5@gmail.com>
2025-01-29 11:24:51 +0100

    print_some_images

--- code_for_stuff.sh
+++ code_for_stuff.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+export DETECTRON2_DATASETS=/home/nberardo/Datasets
+export CUDA_HOME=/usr/local/cuda-11.3
+
 python code_for_stuff.py \
   --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_bt.yaml \
   --num-gpus 1
