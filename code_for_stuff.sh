e48cc42c79811f880a4e0c0c48a5b819a4fcf801
nicholas.berardo <berardonicholas5@gmail.com>
2025-01-30 09:00:56 +0100

    print_some_images

--- code_for_stuff.sh
+++ code_for_stuff.sh
@@ -3,9 +3,9 @@
 export DETECTRON2_DATASETS=/home/nberardo/Datasets
 export CUDA_HOME=/usr/local/cuda-11.3
 
-python code_for_stuff.py \
-  --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_bt.yaml \
-  --num-gpus 1
+#python code_for_stuff.py \
+#  --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_bt.yaml \
+#  --num-gpus 1
 
 #python code_for_stuff.py \
 #  --config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
@@ -19,9 +19,9 @@ python code_for_stuff.py \
 #--config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
 #--input /home/nberardo/Datasets/RoadObsticle21/images/*.webp \
 
-#python code_for_stuff.py \
-#--config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
-#--input /home/nberardo/Datasets/FS_LostFound_full/images/*.png \
+python code_for_stuff.py \
+--config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
+--input /home/nberardo/Datasets/FS_LostFound_full/images/*.png \
 
 #python code_for_stuff.py \
 #--config-file configs/cityscapes/semantic-segmentation/maskformer2_R50_bs16_90k_inference.yaml \
