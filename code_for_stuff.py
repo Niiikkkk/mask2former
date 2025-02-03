57da7860895689d9f791f41127e3a5b5d5c1ffed
nicholas.berardo <berardonicholas5@gmail.com>
2025-02-03 09:11:52 +0100

    print_some_images

--- code_for_stuff.py
+++ code_for_stuff.py
@@ -254,7 +254,7 @@ def id_lora(args):
     cfg.defrost()
 
     lrs = [8e-5, 6e-5]
-    max_iters = [2000,4000,8000]
+    max_iters = [2000]
     lora_configs = [
         {"name" : "backbone_only",
             "lora_cfg" : get_lora_config_backbone_only()},
