cd25fdf2fa003237fad8e764e8c6faa6001a1ccd
nicholas.berardo <berardonicholas5@gmail.com>
2025-02-04 21:51:05 +0100

    print_some_images

--- evaluation_on_ood.py
+++ evaluation_on_ood.py
@@ -78,7 +78,7 @@ def func(model, args, cfg):
         open(file_path, 'w').close()
     file = open(file_path, 'a')
 
-    db_name = str(args.input[0].split('/')[4])
+    db_name = str(args.input.split('/')[4])
     print(db_name)
     return
     file.write(db_name + "\n")
